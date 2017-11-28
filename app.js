9c6143ac2918ed016825ce3129d161307d0dbcac
KizeRemi <remi.mavillaz@live.fr>
2017-11-28 17:54:54 +0100

    change in package.json

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const keySecret = process.env.SECRET_KEY;
 
 const app = require("express")();
 const stripe = require("stripe")(keySecret);
-
 var port = process.env.PORT || 8080;
 
 app.set("view engine", "pug");
@@ -13,21 +12,22 @@ app.get("/", (req, res) =>
 res.render("index.pug", {keyPublishable}));
 
 app.post("/charge", (req, res) => {
-let amount = 1000;
-
-stripe.customers.create({
-   email: req.body.stripeEmail,
-  source: req.body.stripeToken
-})
-.then(customer =>
-  stripe.charges.create({
-    amount,
-    description: "Sample Charge",
-       currency: "usd",
-       customer: customer.id
-  }))
-.then(charge => res.render("charge.pug"));
-});
+        let amount = req.body.paiementValue;
+        console.log(amount);
+        stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+        })
+        .then(customer =>
+            stripe.charges.create({
+            amount,
+            description: "Sample Charge",
+                currency: "usd",
+                customer: customer.id
+        }))
+        .then(charge => 
+            res.render("index.pug"));
+        });
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
